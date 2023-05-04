06c9a99df3d47f128385261515dba4b17a7b0636
Tuomo Kankaanpää <tuomo@tuomokankaanpaa.com>
2023-05-04 09:14:16 +0300

    Initial commit.

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,16 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import { db } from '@vercel/postgres';
+ 
+export default async function handler(request, response) {
+  const client = await db.connect();
+ 
+  try {
+    await client.sql`CREATE TABLE IF NOT EXISTS Pets ( Name varchar(255), Owner varchar(255) );`;
+    const names = ['Fiona', 'Lucy'];
+    await client.sql`INSERT INTO Pets (Name, Owner) VALUES (${names[0]}, ${names[1]});`;
+  } catch (error) {
+    return response.status(500).json({ error });
+  }
+ 
+  const pets = await client.sql`SELECT * FROM Pets;`;
+  return response.status(200).json({ pets: pets.rows });
 }
